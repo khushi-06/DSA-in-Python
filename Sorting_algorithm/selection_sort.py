be5c585d107fc01b966c4041e1ffda9537985311
khushi gupta <guptakhushi622003@gmail.com>
2023-05-17 10:35:29 +0530

    added the selection sort code

---
+++ Sorting_algorithm/selection_sort.py
@@ -0,0 +1,31 @@
+# here the whole aaray is been traverse and then the minimum no. is found out 
+# and then its swap with the first valuE INDEX AND THE INDEX IS INCREMNETED AND THE SAME PROCESS IS REPATED AGAIN 
+
+
+def selection_sort(A):
+
+
+    for i in range (0,len(A)):
+        min=i
+        
+        for j in range (i+1,len(A)):
+            if A[j]<A[min]:
+                min=j
+                
+
+            temp=A[i]
+            A[i]=A[min]
+            A[min]=temp
+    return A
+    
+        
+ 
+        
+A=[1,22,3,6,4]
+B=selection_sort(A)   
+# Here B is a object that is calling the selection sort function
+print(B)
+
+
+
+
